cd4367286cf2559450f15b38460202ca2ee237df
StéphaneCash <kikonistephane@gmail.com>
2021-10-21 01:49:06 +0200

    DétailSpécialisteOK

--- src/views/Specialistes.js
+++ src/views/Specialistes.js
@@ -159,7 +159,7 @@ function Specialistes() {
                                                 </button>
                                             </td>
 
-                                        </tr >
+                                        </tr>
                                         )
                                     })
                                 }
