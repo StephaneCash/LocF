2c7de86719f4bb4961caa41544e3b7a8c84bfb90
StéphaneCash <kikonistephane@gmail.com>
2021-10-20 03:00:20 +0200

    d

--- src/views/LocSpecialiste.js
+++ src/views/LocSpecialiste.js
@@ -13,7 +13,7 @@ export class MapContainer extends Component {
 
             mapCenter: {
                 lat: -4.441931,
-                lng: 15.266247990900
+                lng: 15.266247657
             }
         };
     }
